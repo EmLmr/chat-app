0aab0347f313400003d0558c5239ad2b677886ad
EmLmr <emeline.coding@gmail.com>
2021-10-22 17:08:55 +0200

    added comments

--- components/Chat.js
+++ components/Chat.js
@@ -4,8 +4,8 @@ import { View, Text } from "react-native";
 //the chat component - the main component that will render the UI
 export default class Chat extends React.Component {
     render() {
+        //holds the user's name, passed as props on the Start screen
         let name = this.props.route.params.name;
-
         this.props.navigation.setOptions({ title: name });
 
         return (
--- components/Start.js
+++ components/Start.js
@@ -3,6 +3,7 @@ import { View, TextInput, Button } from "react-native";
 
 //the start/homepage component
 export default class Start extends React.Component {
+    //starting with an empty state so that user can enter their own name
     constructor(props) {
         super(props);
         this.state = { name: "" };
@@ -16,6 +17,7 @@ export default class Start extends React.Component {
                     value={this.state.name}
                     placeholder="Type your name"
                 />
+                {/*will open the Chat screen + pass the user input (name) as props to appear on the Chat screen*/}
                 <Button
                     title="Go to Chat"
                     onPress={() => this.props.navigation.navigate("Chat", { name: this.state.name })}
