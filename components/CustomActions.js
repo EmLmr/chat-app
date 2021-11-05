b120db1e63724f38392b2765c3d7d96dafbd9dac
EmLmr <emeline.coding@gmail.com>
2021-11-05 10:43:25 +0100

    added full functionalities to action sheet

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
 
 class CustomActions extends Component {
     //actions available to user
@@ -16,31 +20,74 @@ class CustomActions extends Component {
                 switch (buttonIndex) {
                     case 0:
                         console.log('user wants to pick an image');
-                        return pickImage();
+                        return this.pickImage();
                     case 1:
                         console.log('user wants to take a photo');
-                        return;
+                        return this.takePhoto();
                     case 2:
                         console.log('user wants to get their location');
+                        return this.getLocation();
                     default:
                 }
             }
         );
     };
 
+    //let's user pick an image - switch case 0
     pickImage = async () => {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+        try {
+            if (status === 'granted') {
+                const result = await ImagePicker.launchImageLibraryAsync({
+                    mediaTypes: 'Images',
+                }).catch((error) => console.log(error));
 
-        if (status === 'granted') {
-            let result = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: 'Images',
-            }).catch((error) => console.log(error));
-
-            if (!result.cancelled) {
-                this.setState({
-                    image: result,
-                });
+                if (!result.cancelled) {
+                    const imageUrl = await this.uploadImageFetch(result.uri);
+                    this.props.onSend({ image: imageUrl });
+                }
             }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    //let's user take a photo - switch case 1
+    takePhoto = async () => {
+        const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY, Permissions.CAMERA);
+        try {
+            if (status === 'granted') {
+                const result = await ImagePicker.launchCameraAsync().catch((error) => console.log(error));
+
+                if (!result.cancelled) {
+                    const imageUrl = await this.uploadImageFetch(result.uri);
+                    this.props.onSend({ image: imageUrl });
+                }
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    //let's user share their location - switch case 2
+    getLocation = async () => {
+        try {
+            const locationPermission = await Location.requestForegroundPermissionsAsync();
+            if (locationPermission.granted) {
+                const locationResult = await Location.getCurrentPositionAsync().catch((err) => console.error(err));
+                const longitude = JSON.stringify(locationResult.coords.longitude);
+                const latitude = JSON.stringify(locationResult.coords.latitude);
+                if (locationResult) {
+                    this.props.onSend({
+                        location: {
+                            longitude: longitude,
+                            latitude: latitude,
+                        },
+                    });
+                }
+            }
+        } catch (err) {
+            console.error(err.message);
         }
     };
 
